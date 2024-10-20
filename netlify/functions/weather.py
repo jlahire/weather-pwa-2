de784685624f4328586dab918ce917cbd0f8b74d
Michael Pabst <mikeypbr3@gmail.com>
2024-10-20 16:40:03 -0400

    successful locally. updated DEVLOG to match changes made to weather.py.

--- netlify/functions/weather.py
+++ netlify/functions/weather.py
@@ -1,5 +1,16 @@
 import json
 from datetime import datetime, timedelta
+from noaa_sdk import NOAA
+
+
+def get_weather(zipcode, country, period):
+    #timeframe
+    endDate = datetime.now() #today
+    startDate = endDate - timedelta(days=period)
+    n = NOAA()
+    obs = n.get_observations(zipcode, country, start=startDate.strftime('%Y-%m-%d'), end=endDate.strftime('%Y-%m-%d'))
+    
+    return list(obs)
 
 def handler(event, context):
     
@@ -8,9 +19,11 @@ def handler(event, context):
         ws = event.get('weatherStuff',{})
         zipcode = ws.get('zipcode')
         country = ws.get('country')
-        period = ws.get('period')
+        period = int(ws.get('period', 7))
         
-        return {'statusCode': 200,'body': json.dumps({'message': f' {zipcode}, {country}, {period} test.'})}
+        wData = get_weather(zipcode, country, period)
+        
+        return {'statusCode': 200,'body': json.dumps({'message': f' {zipcode}, {country}, {period} test.', 'dataLength' : len(wData)})}
         
     except Exception as e:
         
@@ -18,9 +31,9 @@ def handler(event, context):
         
 
 #test
-test_event = {'weatherStuff': {'zipcode':'30350', 'country':'US', 'period':'14'}}
+test_event = {'weatherStuff': {'zipcode':'30350', 'country':'US', 'period':'7'}}
 result = handler(test_event, {})
 print(result)
 assert result['statusCode'] == 200
-#assert json.loads(result['body'])['message'] == 'hello world'
-print("it works!")
+assert json.loads(result['body'])['dataLength'] > 0
+print('weather fetched successfully')
