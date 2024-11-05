447d6cbd65a488bf12aef845f9662d98d031446b
Michael Pabst <mikeypbr3@gmail.com>
2024-11-05 15:07:16 -0500

    finished the createChart function in app.js. added conversion to farenheit from celcius. changed consol.log to "test#6"

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -20,7 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
             
             if (response.ok) {resultsDiv.textContent = data.weatherData;
                 console.log('Temperature data:', data.temperatureData);
-                console.log('Humidity data:', data.humidityData);} 
+                console.log('Humidity data:', data.humidityData);
+                createChart(data.temperatureData, data.humidityData);} 
+                
                 else {resultsDiv.textContent = `Error: ${data.error || 'could not fetch weather data'}`;}
             } 
         catch (error) {resultsDiv.textContent = `Error: ${error.message}`;console.error('eather fetch error:', error);}
@@ -28,10 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function createChart(temperatureData, humidityData){
-
-
-
+    const weather = document.getElementById('weather-chart');
+    
+    if(window.myChart) { window.myChart.destroy();}
+    
+    window.myChart = new Chart(weather, {type: 'line',data: 
+        {labels: temperatureData.map((_, i) => i + 1),datasets: 
+            [
+                {label: 'Temperature °F',
+                data: temperatureData.map(temp => (temp * 9/5) + 32),
+                borderColor: 'red'},
+                {label: 'Humidity %',
+                data: humidityData,
+                borderColor: 'blue'}
+            ]
+        }
+    });
 }
 
 
-console.log("test #5");
+console.log("test #6");
